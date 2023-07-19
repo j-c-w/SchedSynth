db145aa2c05237486d1a8bd1997f5ac04cad86ae
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2023-07-19 11:59:03 -0400

    Get parser working

--- SchedSynth/src/sketch_parse/parser.rs
+++ SchedSynth/src/sketch_parse/parser.rs
@@ -13,9 +13,11 @@ pub struct Variable {
 
 #[derive(Clone)]
 pub enum SketchAST { // nodes have nesting, <other stuff>
-	Produce(i32, Box<Variable>, Box<SketchAST>), // name, contents
-	For(i32, Box<Variable>, Box<SketchAST>), // variable name, sub-contents
-	Assign(i32, Box<Variable>), // variable name
+	Produce(i32, Variable, Box<SketchAST>), // name, contents
+	For(i32, Variable, Box<SketchAST>), // variable name, sub-contents
+	Assign(i32, Variable), // variable name
+	ASTVariable(i32, Variable), // just a plain variable --- not valid on it's own, but is usefule as
+							// an intermediate wrapper.
 	Sequence(i32, Vec<SketchAST>) // list of sub-asts
 }
 
@@ -24,6 +26,7 @@ pub enum SketchAST { // nodes have nesting, <other stuff>
 trait AST {
     fn children(&self) -> Vec<SketchAST>;
     fn node_type(&self) -> String;
+	fn size(&self) -> i32;
 }
 
 impl ToString for Variable {
@@ -35,10 +38,14 @@ impl ToString for Variable {
 impl ToString for SketchAST {
     fn to_string(&self) -> String {
         match self {
-            SketchAST::Produce(_, n, _) => format!("Produce {}", n.to_string().clone()),
-            SketchAST::For(_, n, _) => format!("For {}", n.to_string().clone()),
+            SketchAST::Produce(_, n, subelts) => format!("Produce {} ({})", n.to_string().clone(),
+				subelts.to_string()),
+            SketchAST::For(_, n, subelts) => format!("For {} ({})", n.to_string().clone(),
+				subelts.to_string()),
             SketchAST::Assign(_, n) => format!("{} = ...", n.to_string().clone()),
-            SketchAST::Sequence(_, _) => format!("Sequence")
+            SketchAST::Sequence(_, subvars) => format!("Sequence({})", subvars.iter().map(|x|
+					x.to_string()).collect::<Vec<String>>().join(",")),
+			SketchAST::ASTVariable(_, name) => format!("Variable {}", name.to_string().clone())
         }
     }
 
@@ -49,19 +56,23 @@ impl AST for SketchAST {
         match self {
             SketchAST::Produce(_nest, _n, children) => {
                 let mut res = Vec::new();
-                res.push(**children);
+                res.push(children.as_ref().clone());
                 res
             },
             SketchAST::For(_nest, _n, children) => {
                 let mut res = Vec::new();
-                res.push(**children);
+                res.push(children.as_ref().clone());
                 res
             },
             SketchAST::Assign(_nest, _n) => {
                 let res = Vec::new();
                 res
             },
-            SketchAST::Sequence(_nest, children) => children.to_vec()
+            SketchAST::Sequence(_nest, children) => children.to_vec(),
+			SketchAST::ASTVariable(_nest, _varname) => {
+				let res = Vec::new();
+				res
+			}
         }
     }
 
@@ -70,48 +81,211 @@ impl AST for SketchAST {
             SketchAST::Produce(_, _, _) => "Produce".into(),
             SketchAST::For(_, _, _) => "For".into(),
             SketchAST::Assign(_, _) => "Assign".into(),
-            SketchAST::Sequence(_, _) => "Sequence".into()
+            SketchAST::Sequence(_, _) => "Sequence".into(),
+			SketchAST::ASTVariable(_, _) => "Variable".into()
+        }
+    }
+
+    fn size(&self) -> i32 {
+        match self {
+            SketchAST::Produce(_, _, child) => child.size() + 1,
+            SketchAST::For(_, _, child) => child.size() + 1,
+            SketchAST::Assign(_, _) => 1,
+            SketchAST::Sequence(_, children) => children.iter().map(|child| child.size()).sum(),
+			SketchAST::ASTVariable(_, _) => 1
         }
     }
 }
 
-fn process(sequence: Pair<Rule>) -> SketchAST {
+fn process(nesting_depth: i32, sequence: Pair<Rule>) -> SketchAST {
+	println!("Processing '{}'", sequence.as_str());
     match sequence.as_rule() {
-        Rule::ident => {
-            println!("Found ident {:?}", sequence.as_str());
-            panic!()
-        }
         Rule::sequence_list => {
+			println!("Got a sequcnce list");
             let mut inner = sequence.into_inner();
-
             // For a sequence, get the head, then go through
             // and get the tail.
             // Note that this produces a very flat list --- it doesn't consider
             // how indented things are --- that is handled in a secondary pass.
-            let seq = process(inner.next().unwrap());
-            if inner.len() > 0 {
+            let seq = process(nesting_depth, inner.next().unwrap());
+            if inner.len() > 1 {
                 let _ = inner.next(); // Whitespace
-                let rest = process(inner.next().unwrap()); // next seq list
-                let existing_children = rest.children();
+				// no increase in nesting depth because seq is just a sequence --it's explicit from
+				// the indentation anyway.
+
+                let rest = process(nesting_depth, inner.next().unwrap()); // next seq list
+                let mut existing_children = rest.children();
                 existing_children.insert(0, seq);
-                SketchAST::Sequence(0, existing_children)
+                SketchAST::Sequence(nesting_depth, existing_children)
             } else {
-                let res_vec = Vec::new();
+				// just sequnce, EOI
+                let mut res_vec = Vec::new();
                 res_vec.push(seq);
-                SketchAST::Sequence(0, res_vec)
+                SketchAST::Sequence(nesting_depth, res_vec)
             }
-        }
+        },
         Rule::sequence => {
-            let mut _inner = panic!();
-        }
-        _ => panic!()
+			println!("Got a sequence");
+			let mut inner = sequence.into_inner();
+
+			assert!(inner.len() == 2);
+
+			let nesting = inner.next().unwrap();
+			let new_nesting_depth: i32 = nesting.into_inner().len() as i32; // Get the depth of the nest
+
+			// nesting despth is explitict from the indentation.
+			let stmt = process(new_nesting_depth, inner.next().unwrap());
+			stmt
+        },
+		Rule::produce => {
+			println!("Got a produce");
+			let mut inner = sequence.into_inner();
+
+			let _title = inner.next(); // produce
+			// let _ = inner.next(); // whitespace
+			let ident = match process(nesting_depth, inner.next().unwrap()) {
+				SketchAST::ASTVariable(_, n) => n,
+			_ => panic!("Unexpected non varaiblae")
+			}; // identifier.
+
+			// Parser produces un-nested code --- nest it later.
+			SketchAST::Produce(nesting_depth, ident,
+				Box::new(SketchAST::Sequence(nesting_depth, Vec::new())))
+		},
+		Rule::pfor => {
+			println!("Got a for");
+			let mut inner = sequence.into_inner();
+
+			let _title = inner.next(); // for
+			// let _ = inner.next(); // whitespace
+			let ident = match process(nesting_depth, inner.next().unwrap()) {
+				SketchAST::ASTVariable(_, n) => n,
+			_ => panic!("Unexpected non varaiblae")
+			}; // identifier.
+
+			// Parser produces un-nested code --- nest it later.
+			SketchAST::For(nesting_depth, ident,
+				Box::new(SketchAST::Sequence(nesting_depth, Vec::new())))
+		},
+		Rule::assignment => {
+			println!("Got an assignment");
+			let mut inner = sequence.into_inner();
+
+			let ident = match process(nesting_depth, inner.next().unwrap()) {
+				SketchAST::ASTVariable(_, n) => n,
+			_ => panic!("Unexpected non varaiblae")
+			}; // identifier.
+
+			SketchAST::Assign(nesting_depth, ident)
+		}
+		Rule::ident => {
+			println!("Got an ident");
+			let name = sequence.as_str();
+			SketchAST::ASTVariable(nesting_depth, Variable{name: name.into()})
+		},
+		Rule::EOI => {
+			println!("Got an EOI");
+			SketchAST::Sequence(nesting_depth, Vec::new())
+		}
+		Rule::indent_nest => {
+			panic!("Unreachable?")
+		},
+		Rule::newline => panic!("Got newline"),
+		Rule::whitespace_plus => panic!("whitespace_plus"),
+		Rule::whitespace => panic!("whitespace"),
+		_ => {
+			panic!("Unexpected rule {}", sequence.as_str())
+		}
     }
 }
 
+// Get the nesting depth
+fn get_nest_depth(v: &SketchAST) -> &i32 {
+	match v {
+		SketchAST::Produce(n, _, _) => n,
+		SketchAST::For(n, _, _) => n,
+		SketchAST::Assign(n, _) => n,
+		SketchAST::Sequence(n, _) => n,
+		SketchAST::ASTVariable(n, _) => n
+	}
+}
+
+// set the loop nest contents
+fn set_nest(v: &SketchAST, nest: SketchAST) -> SketchAST {
+	match v {
+		SketchAST::Produce(n, var, current_nest) => {
+			assert!(current_nest.size() == 0); // check we aren't deleting anything
+			SketchAST::Produce(n.clone(), var.clone(), Box::new(nest))
+		},
+		SketchAST::For(n, var, current_nest) => {
+			assert!(current_nest.size() == 0); // check we aren't deleting anything
+			SketchAST::For(n.clone(), var.clone(), Box::new(nest))
+		}
+		SketchAST::Assign(_n, _var) => panic!("Can't set nest to an assign"),
+		SketchAST::Sequence(_n, _nest) => panic!("Can't set nest to a sequence"),
+		SketchAST::ASTVariable(_n, _var) => panic!("Can't set nest to a variable")
+	}
+}
+
+fn nest_rules(rules: Vec<SketchAST>) -> Vec<SketchAST> {
+	match rules.len() {
+		0 => vec![],
+		1 => rules,
+		_n => {
+			let (head_array, rest) = rules.split_at(1);
+			let head = head_array[0].clone();// will always have one element.
+			let head_nesting = get_nest_depth(&head);
+
+			// Get the things that belong to head and the things that are at
+			// the same level as head.
+			// head index is the index where we return to the nesting of this
+			// loop --- everything that follows belongs at the same
+			// nesting level.
+			let head_index = rest.iter().position(|x| get_nest_depth(x) <= head_nesting);
+			let (before, after) = match head_index {
+				Some(head_index) => rest.split_at(head_index),
+				// if no place where we returned to a lesser nesting,
+				// there is nothing that comes back above the previous statement.
+				None => rest.split_at(rest.len())
+			};
+			
+			// Recursively build structures in the nest:
+			let structured_subnest = nest_rules(before.to_vec());
+			// recurviesvley build structures in the tail:
+			let mut structured_rest = nest_rules(after.to_vec());
+
+			// Set the sub-slements of this item appropriately:
+			let nested = set_nest(&head, SketchAST::Sequence(head_nesting + 1, structured_subnest));
+			// And put this at the start of the nest for this level.
+			structured_rest.insert(0, nested);
+			structured_rest
+		}
+	}
+}
+
+fn nest(parsed: SketchAST) -> SketchAST {
+	match parsed {
+		SketchAST::Sequence(0, rules) => {
+			SketchAST::Sequence(0, nest_rules(rules))
+		}
+		_ => panic!("Unexepcted top-level statement {}", parsed.to_string())
+	}
+}
+
 pub fn parse() {
-    let mut sequence = LoopParser::parse(Rule::sequence_list, "produce abd").unwrap();
-    process(sequence.next().unwrap());
-    for token in sequence.tokens() {
-        println!("Token: {:?}", token);
-    }
+	let input = r#"produce test:
+  for x:
+    for y:
+      test(...) = ...
+    for z:
+      test(...) = ..."#;
+	let simple = "    test(...) = ...";
+    let mut sequence = LoopParser::parse(Rule::sequence_list, input).unwrap();
+	// Parse into a flat structure.
+    let parsed = process(0, sequence.next().unwrap());
+	println!("Parsed {}", parsed.to_string());
+
+	// Go through and properly nest everything.
+	let nested = nest(parsed);
+	println!("Nested {}", nested.to_string());
 }
