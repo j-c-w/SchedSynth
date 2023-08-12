128fd306a6a03c0de2f406b6003cf0fc79ffc818
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2023-08-11 21:03:47 -0400

    Add more deps to shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
 { pkgs ? import<nixpkgs> {} }:
 
 with pkgs;
-mkShell {
-	buildInputs = [ rustc cargo ];
+mkShell rec {
+	buildInputs = [ gcc rustc cargo zlib.dev zlib ];
 	SHELL_NAME = "SchedSynth";
+
+	shellHook = ''
+		export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+		export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+	'';
+
 }
